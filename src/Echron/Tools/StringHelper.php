aa0f475d30393f4b49618e843d888b054c928175
Stijn Duynslaeger <info@echron.eu>
2016-11-06 16:35:33 +0100

    Make stringhelper functions static Add string masking function (usefull for passwords)

--- src/Echron/Tools/StringHelper.php
+++ src/Echron/Tools/StringHelper.php
@@ -5,8 +5,27 @@ namespace Echron\Tools;
 
 class StringHelper
 {
-    function startsWith(string $haystack, string $needle):bool
+    public static function startsWith(string $haystack, string $needle):bool
     {
         return $needle === '' || strrpos($haystack, $needle, -strlen($haystack)) !== false;
+    }
+
+    public static function mask(string $target, string $mask = '*', int $showStartCharacters = 2, int $showEndCharacters = 2):string
+    {
+        $length = strlen($target);
+        $masked = str_repeat($mask, $length);
+
+        if ($showStartCharacters < 0) {
+            $showStartCharacters = 0;
+        }
+        if ($showEndCharacters < 0) {
+            $showEndCharacters = 0;
+        }
+
+        if ($length > ($showStartCharacters + $showEndCharacters + 2)) {
+            $masked = substr($target, 0, $showStartCharacters) . '' . substr($masked, $showStartCharacters, -$showEndCharacters) . '' . substr($target, -$showEndCharacters, $showEndCharacters);
+        }
+
+        return $masked;
     }
 }

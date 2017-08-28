65375803fe39a564c88d59571505acd713d28701
Echron <stijn@echron.be>
2017-08-28 19:03:11 +0200

    Improve parsing of memory strings + unittest

--- src/Echron/Tools/Memory.php
+++ src/Echron/Tools/Memory.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Echron\Tools;
 
@@ -12,30 +13,31 @@ class Memory
     public static function getBytesLimit(): int
     {
         $memory_limit = ini_get('memory_limit');
-        $memory_limit = trim($memory_limit);
-        $last = strtolower($memory_limit[strlen($memory_limit) - 1]);
+
+        $memory_limit = self::parseMemoryString($memory_limit);
+
+        return $memory_limit;
+
+    }
+
+    private static function parseMemoryString(string $memoryString): int
+    {
+        $memoryString = trim($memoryString);
+        $last = strtolower($memoryString[strlen($memoryString) - 1]);
         switch ($last) {
             // The 'G' modifier is available since PHP 5.1.0
             case 'g':
-                $memory_limit *= 1024;
+                $memoryString *= 1024 * 1024 * 1024;
                 break;
             case 'm':
-                $memory_limit *= 1024;
+                $memoryString *= 1024 * 1024;
                 break;
             case 'k':
-                $memory_limit *= 1024;
+                $memoryString *= 1024;
                 break;
         }
 
-        return $memory_limit;
-//        if (preg_match('/^(\d+)(.)$/', $memory_limit, $matches)) {
-//            if ($matches[2] == 'M') {
-//                $memory_limit = $matches[1] * 1024 * 1024; // nnnM -> nnn MB
-//            } else if ($matches[2] == 'K') {
-//                $memory_limit = $matches[1] * 1024; // nnnK -> nnn KB
-//            }
-//        }
-
+        return $memoryString;
     }
 
 }

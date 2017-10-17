31a4c5a887ed0bfeef9769484728ff371ca398f1
Echron <stijn@echron.be>
2017-10-17 14:46:27 +0200

    Add VarHelper (get type of a variable, get class name when object)

---
+++ src/Echron/Tools/VarHelper.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Echron\Tools;
+
+class VarHelper
+{
+    public static function getType($var, bool $returnClassIfObject = true): string
+    {
+        $type = gettype($var);
+        if ($type === 'object' && $returnClassIfObject) {
+            $type = get_class($var);
+        }
+
+        return $type;
+    }
+}

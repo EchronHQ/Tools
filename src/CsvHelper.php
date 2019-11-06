f8c8a2938fc61fe589e1aaf6e481b2e89d7dbff2
Echron <stijn@echron.be>
2019-11-06 18:16:53 +0100

    CSV helper: add separate line delimiter property + allow to pass headers

--- src/CsvHelper.php
+++ src/CsvHelper.php
@@ -20,39 +20,17 @@ class CsvHelper
 
         $result = [];
 
-        // $headers = null;
         $row = 1;
 
-        //        $fileData = \str_replace([
-        //            "\r\n",
-        //            //                    "\r",
-        //        ], \PHP_EOL, $fileData);
-
-        //        $fileData = \str_replace([
-        //            //            "\r\n",
-        //            "\r",
-        //        ], \PHP_EOL, $fileData);
         $lines = \str_getcsv($fileData, $lineDelimiter);
-
-        //        foreach ($lines as $line) {
-        //            var_dump($line);
-        //        }
-        //        die('---');
-
-        // return [];
 
         foreach ($lines as $line) {
             $lineData = \str_getcsv($line, $delimiter);
-
-            //            var_dump($lineData);
-            //            var_dump($line);
-            //            die('---');
 
             if (\is_null($headers)) {
                 $headers = $lineData;
             } else {
                 $num = count($lineData);
-                //                    echo "<p> $num fields in line $row: <br /></p>\n";
                 $row++;
 
                 $productData = [];
@@ -63,41 +41,10 @@ class CsvHelper
                     }
 
                     $productData[$field] = $lineData[$c];
-                    //                        echo $field . ': ' . $data[$c] . \PHP_EOL;
                 }
 
                 $result[] = $productData;
             }
-        }
-
-        return $result;
-        var_dump($result);
-        die('---');
-
-        if (($handle = fopen($filePath, "r")) !== false) {
-            while (($lineData = fgetcsv($handle, 4096, $delimiter)) !== false) {
-                if (\is_null($headers)) {
-                    $headers = $lineData;
-                } else {
-                    $num = count($lineData);
-                    //                    echo "<p> $num fields in line $row: <br /></p>\n";
-                    $row++;
-
-                    $productData = [];
-                    for ($c = 0; $c < $num; $c++) {
-                        $field = '[unknown ' . $c . ']';
-                        if (isset($headers[$c])) {
-                            $field = $headers[$c];
-                        }
-
-                        $productData[$field] = $lineData[$c];
-                        //                        echo $field . ': ' . $data[$c] . \PHP_EOL;
-                    }
-
-                    $result[] = $productData;
-                }
-            }
-            fclose($handle);
         }
 
         return $result;

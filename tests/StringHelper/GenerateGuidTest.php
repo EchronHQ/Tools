e21658bb5071352e6ce91d6d3f7cbd47cbcac2a8
Stijn Duynslaeger <stijn@echron.com>
2023-12-01 13:15:01 +0000

    Fix tests

--- tests/StringHelper/GenerateGuidTest.php
+++ tests/StringHelper/GenerateGuidTest.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Echron\Tools\StringHelper;
 
-class GenerateGuidTest extends \PHPUnit\Framework\TestCase
+use Echron\Tools\StringHelper;
+use PHPUnit\Framework\TestCase;
+
+class GenerateGuidTest extends TestCase
 {
     public function testNormal()
     {
-        $result = \Echron\Tools\StringHelper::generateGuid();
-        var_dump($result);
-        echo $result;
-        // $this->assertStringMatchesFormatFile($result);
+        $result = StringHelper::generateGuid();
+        $this->assertEquals(strlen('b3e9b8b7-a046-4b5e-93c8-364e29420cbe'), strlen($result));
     }
 
 }
